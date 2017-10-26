4bab1e3f9333543205e79184f49949bd2ace8090
Dany Marcoux <danymarcoux@gmail.com>
2017-10-26 23:57:03 +0200

    Raise exception if env not set

--- __main__.py
+++ __main__.py
@@ -2,12 +2,16 @@ import json
 import os
 import requests
 
+auth_token = os.getenv('GITHUB_OAUTH_TOKEN')
+if auth_token is None:
+    raise ValueError('Set environment variable GITHUB_OAUTH_TOKEN')
+
+headers = {"Authorization": "token %s" % auth_token}
+
 # TODO: Get owner and name from STDIN, then use it in json below
 owner = "dmarcoux"
 name = "grc"
 json = {'query': '{ repository(owner: "facebook", name: "graphql") { releases(last: 1) { nodes { tag { name } } } } }'}
-# TODO: Error out if GITHUB_OAUTH_TOKEN is not set
-headers = {"Authorization": "token %s" % os.environ['GITHUB_OAUTH_TOKEN']}
 
 r = requests.post(url='https://api.github.com/graphql', json=json, headers=headers)
 # TODO: Check if there's a node before fetching name, otherwise out of range error

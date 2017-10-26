a0debca3f093595dd33068672b538f75bf7fef55
Dany Marcoux <danymarcoux@gmail.com>
2017-10-26 19:05:12 +0200

    Use tag name instead of release name

--- __main__.py
+++ __main__.py
@@ -5,11 +5,10 @@ import requests
 # TODO: Get owner and name from STDIN, then use it in json below
 owner = "dmarcoux"
 name = "grc"
-# TODO: Check if nodes { name } is really want we need
-json = {'query': '{ repository(owner: "facebook", name: "graphql") { releases(last: 1) { nodes { name } } } }'}
+json = {'query': '{ repository(owner: "facebook", name: "graphql") { releases(last: 1) { nodes { tag { name } } } } }'}
 # TODO: Error out if GITHUB_OAUTH_TOKEN is not set
 headers = {"Authorization": "token %s" % os.environ['GITHUB_OAUTH_TOKEN']}
 
 r = requests.post(url='https://api.github.com/graphql', json=json, headers=headers)
 # TODO: Check if there's a node before fetching name, otherwise out of range error
-print(r.json()['data']['repository']['releases']['nodes'][0]['name'])
+print(r.json()['data']['repository']['releases']['nodes'][0]['tag']['name'])
